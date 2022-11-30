b578b07c8c503311e72d538bdfa2ac2a2b89c601
scroopy__noopers <90370221+shrimpleypibbles@users.noreply.github.com>
2022-11-30 13:32:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import art
+
+alphabet = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+]
+print(art.logo)
+
+
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position % 26]
+        else:
+            end_text += char
+    print(f"The {direction}d text is {end_text}\n")
+
+
+should_continue = True
+while should_continue == True:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+    again = input("Would you like to run the cypher again? Type yes or no. \n")
+    if again == "no":
+        should_continue = False
+        print("...good night, and good luck.")
+
+#TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
+#Try running the program and entering a shift number of 45.
+#Add some code so that the program continues to work even if the user enters a shift number greater than 26.
+#Hint: Think about how you can use the modulus (%).
